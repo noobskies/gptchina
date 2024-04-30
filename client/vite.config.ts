aa2843bf35ed3fa3fe0bb62cde7e53e2d480693e
noobskie <t.noobskie@gmail.com>
2024-04-30 13:51:15 -0400

    fix merge

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,30 @@
 import path, { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig, createLogger } from 'vite';
+import { defineConfig, createLogger, loadEnv } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import type { Plugin } from 'vite';
+
+function htmlPlugin(env: ReturnType<typeof loadEnv>) {
+  return {
+    name: 'html-transform',
+    transformIndexHtml: {
+      enforce: 'pre' as const,
+      transform: (html: string): string => {
+        return html.replace(/%(.*?)%/g, (match, p1) => {
+          const value = env[p1];
+          if (value === undefined) {
+            return match;
+          }
+          if (!value.includes('"')) {
+            return value;
+          }
+          return value.replace(/"/g, '&quot;');
+        });
+      },
+    },
+  };
+}
 
 const logger = createLogger();
 const originalWarning = logger.warn;
@@ -27,119 +48,120 @@ logger.warn = (msg, options) => {
 };
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  customLogger: logger,
-  server: {
-    fs: {
-      cachedChecks: false,
-    },
-    host: 'localhost',
-    port: 3090,
-    strictPort: false,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3080',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    customLogger: logger,
+    server: {
+      fs: {
+        cachedChecks: false,
       },
-      '/oauth': {
-        target: 'http://localhost:3080',
-        changeOrigin: true,
-      },
-    },
-  },
-  // All other env variables are filtered out
-  envDir: '../',
-  envPrefix: ['VITE_', 'SCRIPT_', 'DOMAIN_', 'ALLOW_'],
-  plugins: [
-    react(),
-    nodePolyfills(),
-    VitePWA({
-      injectRegister: 'auto', // 'auto' | 'manual' | 'disabled'
-      registerType: 'prompt', // 'prompt' | 'auto' | 'disabled'
-      devOptions: {
-        enabled: false, // enable/disable registering SW in development mode
-      },
-      workbox: {
-        globPatterns: ['assets/**/*.{png,jpg,svg,ico}', '**/*.{js,css,html,ico,woff2}'],
-        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
-      },
-      manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#000000',
-        theme_color: '#009688',
-        icons: [
-          {
-            src: '/assets/favicon-32x32.png',
-            sizes: '32x32',
-            type: 'image/png',
-          },
-          {
-            src: '/assets/favicon-16x16.png',
-            sizes: '16x16',
-            type: 'image/png',
-          },
-          {
-            src: '/assets/apple-touch-icon-180x180.png',
-            sizes: '180x180',
-            type: 'image/png',
-          },
-          {
-            src: '/assets/maskable-icon.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-        ],
-      },
-    }),
-    sourcemapExclude({ excludeNodeModules: true }),
-  ],
-  publicDir: './public',
-  build: {
-    sourcemap: process.env.NODE_ENV === 'development',
-    outDir: './dist',
-    rollupOptions: {
-      // external: ['uuid'],
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules/highlight.js')) {
-            return 'markdown_highlight';
-          }
-          if (id.includes('node_modules/hast-util-raw')) {
-            return 'markdown_large';
-          }
-          if (id.includes('node_modules/katex')) {
-            return 'markdown_large';
-          }
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
+      host: 'localhost',
+      port: 3090,
+      strictPort: false,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3080',
+          changeOrigin: true,
+        },
+        '/oauth': {
+          target: 'http://localhost:3080',
+          changeOrigin: true,
         },
       },
-      /**
-       * Ignore "use client" waning since we are not using SSR
-       * @see {@link https://github.com/TanStack/query/pull/5161#issuecomment-1477389761 Preserve 'use client' directives TanStack/query#5161}
-       */
-      onwarn(warning, warn) {
-        if (
-          // warning.code === 'MODULE_LEVEL_DIRECTIVE' &&
-          warning.message.includes('Error when using sourcemap')
-        ) {
-          return;
-        }
-        warn(warning);
+    },
+    // All other env variables are filtered out
+    envDir: '../',
+    envPrefix: ['VITE_', 'SCRIPT_', 'DOMAIN_', 'ALLOW_'],
+    plugins: [
+      react(),
+      nodePolyfills(),
+      VitePWA({
+        injectRegister: 'auto', // 'auto' | 'manual' | 'disabled'
+        registerType: 'prompt', // 'prompt' | 'auto' | 'disabled'
+        devOptions: {
+          enabled: false, // enable/disable registering SW in development mode
+        },
+        workbox: {
+          globPatterns: ['assets/**/*.{png,jpg,svg,ico}', '**/*.{js,css,html,ico,woff2}'],
+          maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
+        },
+        manifest: {
+          name: 'LibreChat',
+          short_name: 'LibreChat',
+          start_url: '/',
+          display: 'standalone',
+          background_color: '#000000',
+          theme_color: '#009688',
+          icons: [
+            {
+              src: '/assets/favicon-32x32.png',
+              sizes: '32x32',
+              type: 'image/png',
+            },
+            {
+              src: '/assets/favicon-16x16.png',
+              sizes: '16x16',
+              type: 'image/png',
+            },
+            {
+              src: '/assets/apple-touch-icon-180x180.png',
+              sizes: '180x180',
+              type: 'image/png',
+            },
+            {
+              src: '/assets/maskable-icon.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable',
+            },
+          ],
+        },
+      }),
+      sourcemapExclude({ excludeNodeModules: true }),
+      htmlPlugin(env),
+    ],
+    publicDir: './public',
+    build: {
+      sourcemap: process.env.NODE_ENV === 'development',
+      outDir: './dist',
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('node_modules/highlight.js')) {
+              return 'markdown_highlight';
+            }
+            if (id.includes('node_modules/hast-util-raw')) {
+              return 'markdown_large';
+            }
+            if (id.includes('node_modules/katex')) {
+              return 'markdown_large';
+            }
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+        },
+        /**
+         * Ignore "use client" waning since we are not using SSR
+         * @see {@link https://github.com/TanStack/query/pull/5161#issuecomment-1477389761 Preserve 'use client' directives TanStack/query#5161}
+         */
+        onwarn(warning, warn) {
+          if (warning.message.includes('Error when using sourcemap')) {
+            return;
+          }
+          warn(warning);
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '~': path.join(__dirname, 'src/'),
-      $fonts: resolve('public/fonts'),
+    resolve: {
+      alias: {
+        '~': path.join(__dirname, 'src/'),
+        $fonts: resolve('public/fonts'),
+      },
     },
-  },
+  };
 });
 
 interface SourcemapExclude {
