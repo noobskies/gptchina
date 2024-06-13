ad0031ca6faa9862efe3675e69e08392efaeb282
noobskie <t.noobskie@gmail.com>
2024-06-13 15:48:54 -0400

    ionic

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.novlisky.app',
   appName: 'Novlisky',
-  webDir: 'client/dist',
+  webDir: '/client/dist',
 };
 
 export default config;
