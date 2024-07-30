07e3965a90c2b32407d744dba38e72634ca5b070
noobskie <t.noobskie@gmail.com>
2024-07-30 11:25:26 -0500

    client

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.novlisky.twa',
+  appName: 'Novlisky',
+  webDir: 'client/dist',
+};
+
+export default config;
