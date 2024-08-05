65cadcd05ef1700ba405247da5827bb3cae218cd
NoobSkie <t.noobskie@gmail.com>
2024-08-05 16:08:59 -0500

    mobile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,33 @@ const config: CapacitorConfig = {
   appId: 'io.novlisky.twa',
   appName: 'Novlisky',
   webDir: 'client/dist',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: '#eeeeee',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: true,
+      androidSpinnerStyle: 'large',
+      iosSpinnerStyle: 'small',
+      spinnerColor: '#999999',
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: 'launch_screen',
+      useDialog: true,
+    },
+  },
+  android: {
+    backgroundColor: '#eeeeee',
+  },
+  ios: {
+    backgroundColor: '#eeeeee',
+  },
+  server: {
+    androidScheme: 'https',
+  },
 };
 
 export default config;
