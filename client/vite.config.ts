70470777743e3e1e73087ea0119bb54869968776
NoobSkie <t.noobskie@gmail.com>
2024-09-04 15:31:21 -0500

    fix safe

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -151,10 +151,6 @@ export default defineConfig(({ mode }) => {
             }
           },
         },
-        /**
-         * Ignore "use client" waning since we are not using SSR
-         * @see {@link https://github.com/TanStack/query/pull/5161#issuecomment-1477389761 Preserve 'use client' directives TanStack/query#5161}
-         */
         onwarn(warning, warn) {
           if (warning.message.includes('Error when using sourcemap')) {
             return;
