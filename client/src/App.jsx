6f3cef5d635fd888d39cf3721882f1c725b5306e
NoobSkie <t.noobskie@gmail.com>
2024-09-14 13:07:32 -0500

    fix dark/light text

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -66,14 +66,12 @@ const App = () => {
             <RadixToast.Provider>
               <ToastProvider>
                 <DndProvider backend={HTML5Backend}>
-                  <div className="min-h-screen flex flex-col safe-y">
-                    <RouterProvider router={router}>
-                      <PageViewTracker />
-                    </RouterProvider>
-                    <ReactQueryDevtools initialIsOpen={false} position="top-right" />
-                    <Toast />
-                    <RadixToast.Viewport className="pointer-events-none fixed inset-0 z-[1000] mx-auto my-2 flex max-w-[560px] flex-col items-stretch justify-start md:pb-5" />
-                  </div>
+                  <RouterProvider router={router}>
+                    <PageViewTracker />
+                  </RouterProvider>
+                  <ReactQueryDevtools initialIsOpen={false} position="top-right" />
+                  <Toast />
+                  <RadixToast.Viewport className="pointer-events-none fixed inset-0 z-[1000] mx-auto my-2 flex max-w-[560px] flex-col items-stretch justify-start md:pb-5" />
                 </DndProvider>
               </ToastProvider>
             </RadixToast.Provider>
