cde1905e8e31a31dc075166c57cef7a57f71d319
NoobSkie <t.noobskie@gmail.com>
2024-09-23 19:47:58 -0500

    fix text

--- client/src/routes/Layouts/DashBreadcrumb.tsx
+++ client/src/routes/Layouts/DashBreadcrumb.tsx
@@ -56,7 +56,7 @@ export default function DashBreadcrumb() {
 
   return (
     <div className="mr-4 flex h-10 items-center justify-between">
-      <Breadcrumb className="mt-1 px-2 dark:text-gray-200">
+      <Breadcrumb className="mt-1 px-2 text-gray-800 dark:text-gray-200">
         <BreadcrumbList>
           <BreadcrumbItem className="hover:dark:text-white">
             <BreadcrumbLink
