5b9cfff17ff4775bd575dd6ae8aff8b47bc35c60
NoobSkie <t.noobskie@gmail.com>
2024-09-25 12:23:41 -0500

    version

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.novlisky.twa',
+  appId: 'twa.novlisky.io',
   appName: 'Novlisky',
   webDir: 'client/dist',
   bundledWebRuntime: false,
