1afab2c41c94970f3750d256cbb3e93eaf77a650
NoobSkie <t.noobskie@gmail.com>
2024-11-27 15:38:49 -0600

    remove inapp restriction

--- api/server/routes/payment/opennode.js
+++ api/server/routes/payment/opennode.js
@@ -6,10 +6,10 @@ const { requireJwtAuth } = require('../../middleware');
 
 router.post('/create-charge', requireJwtAuth, OpenNodeController.createCharge);
 router.get('/charge/:chargeId', requireJwtAuth, OpenNodeController.getCharge);
-router.post(
-  '/webhook',
-  express.raw({ type: 'application/json' }),
-  OpenNodeController.handleWebhook,
-);
+router.post('/webhook', express.raw({ type: 'application/json' }), (req, res) => {
+  console.log('Webhook received:', req.body);
+  console.log('Webhook headers:', req.headers);
+  OpenNodeController.handleWebhook(req, res);
+});
 
 module.exports = router;
