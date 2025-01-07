a0be35c76cd7e641cc9637e4d2844b794665c8be
NoobSkie <t.noobskie@gmail.com>
2025-01-06 20:44:33 -0600

    update token burn rate

--- client/src/components/ui/Footer.tsx
+++ client/src/components/ui/Footer.tsx
@@ -7,16 +7,26 @@ const Footer = () => {
 
   return (
     <footer className="flex flex-col items-center justify-center space-y-8 pb-10">
-      <div className="flex flex-col items-center space-y-3 text-gray-400 sm:flex-row sm:space-x-8 sm:space-y-0">
-        <Link to="/privacy-policy" className="hover:text-blue-600">
+      <div className="flex flex-col items-center space-y-3 text-gray-400 dark:text-gray-500 sm:flex-row sm:space-x-8 sm:space-y-0">
+        <Link
+          to="/privacy-policy"
+          className="transition-colors hover:text-blue-600 dark:hover:text-blue-400"
+        >
           Privacy Policy
         </Link>
-        <Link to="/terms-of-service" className="hover:text-blue-600">
+        <Link
+          to="/terms-of-service"
+          className="transition-colors hover:text-blue-600 dark:hover:text-blue-400"
+        >
           Terms of Service
         </Link>
       </div>
       <Link to="/">
-        <img src={domainData.logoPath} alt={`${domainData.logoText} Logo`} className="h-8 w-8" />
+        <img
+          src={domainData.smallLogoPath}
+          alt={`${domainData.logoText} Logo`}
+          className="h-8 w-8"
+        />
       </Link>
     </footer>
   );
--- client/src/components/ui/Header.tsx
+++ client/src/components/ui/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   const domainData = getDomainData();
 
   return (
-    <header className="sticky top-0 z-10 bg-white/40 px-5 py-4 backdrop-blur-lg lg:bg-transparent lg:px-10 lg:backdrop-blur-none">
+    <header className="sticky top-0 z-10 bg-white/40 px-5 py-4 backdrop-blur-lg dark:bg-gray-900/40 lg:bg-transparent lg:px-10 lg:backdrop-blur-none">
       <nav className="mx-auto flex items-center justify-between">
         <div className="flex items-center gap-2">
           <Link to="/" className="flex items-center gap-2">
@@ -15,7 +15,9 @@ const Header = () => {
               alt={`${domainData.logoText} Logo`}
               className="h-8 w-8"
             />
-            <span className="text-xl font-semibold">{domainData.logoText}</span>
+            <span className="text-xl font-semibold text-gray-900 dark:text-gray-50">
+              {domainData.logoText}
+            </span>
           </Link>
         </div>
       </nav>
