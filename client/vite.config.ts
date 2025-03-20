4c7e1bf54c8990a3215889277088e2987bba0fc4
NoobSkie <t.noobskie@gmail.com>
2025-03-20 14:03:44 -0500

    naming

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -44,8 +44,8 @@ export default defineConfig({
       },
       includeAssets: ['**/*'],
       manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
+        name: 'Novlisky',
+        short_name: 'Novlisky',
         start_url: '/',
         display: 'standalone',
         background_color: '#000000',
