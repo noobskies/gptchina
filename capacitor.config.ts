4adc663b427304539ef855215a91ff6663b4fd6d
NoobSkie <t.noobskie@gmail.com>
2025-04-18 17:15:38 -0500

    capacitor android init

---
+++ capacitor.config.ts
@@ -0,0 +1,45 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'twa.novlisky.io',
+  appName: 'Novlisky',
+  webDir: 'client/dist',
+  plugins: {
+    App: {},
+    StatusBar: {
+      style: 'dark',
+      backgroundColor: '#eeeeee',
+      overlays: false,
+    },
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: '#eeeeee',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: true,
+      androidSpinnerStyle: 'large',
+      iosSpinnerStyle: 'small',
+      spinnerColor: '#999999',
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: 'launch_screen',
+      useDialog: true,
+    },
+  },
+  android: {
+    backgroundColor: '#eeeeee',
+  },
+  ios: {
+    backgroundColor: '#eeeeee',
+  },
+  server: {
+    androidScheme: 'https',
+    url: 'https://novlisky.io',
+    // androidScheme: 'http',
+    // cleartext: true,
+    // url: 'http://192.168.0.167:3090',
+  },
+};
+
+export default config;
---
+++ client/android/app/src/main/java/twa/novlisky/io/MainActivity.java
@@ -0,0 +1,5 @@
+package twa.novlisky.io;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
